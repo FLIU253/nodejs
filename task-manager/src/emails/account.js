9a6a45f75f4aac79b71fc6c0500b77082d4fa421
Franklin Liu <franklin.liu@autodata.net>
2019-08-08 13:14:04 -0400

    finished email sending section

--- task-manager/src/emails/account.js
+++ task-manager/src/emails/account.js
@@ -3,10 +3,25 @@ const sendgridAPIKey = require('./keys')
 
 sgMail.setApiKey(sendgridAPIKey.key)
 
-sgMail.send({
-    to: 'franklin_liu3@hotmail.com',
-    from: 'franklin_liu3@hotmail.com',
-    subject: 'This is my first creation',
-    text: 'testing testing testing'
-})
+const sendWelcomeEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'franklin_liu3@hotmail.com',
+        subject: 'Thanks for joining in!',
+        text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
+    })
+}
 
+const sendCancellationEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'franklin_liu3@hotmail.com',
+        subject: 'Sorry to see you go!',
+        text: `Goodbye ${name}. Hope to see you back soon!`
+    })
+}
+
+module.exports = {
+    sendWelcomeEmail,
+    sendCancellationEmail
+}
